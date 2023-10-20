247bff99f905168285cc223c52caa2d9c76fefe6
Seggan <seggan21@gmail.com>
2023-10-19 21:07:43 -0400

    Start on docs

--- app/src/main/kotlin/io/github/seggan/metis/app/SyntaxHighlighting.kt
+++ app/src/main/kotlin/io/github/seggan/metis/app/SyntaxHighlighting.kt
@@ -2,29 +2,41 @@ package io.github.seggan.metis.app
 
 import io.github.seggan.metis.parsing.Token
 import io.github.seggan.metis.parsing.Token.Type.*
-import kotlinx.html.classes
-import kotlinx.html.code
 import kotlinx.html.span
 import kotlinx.html.stream.createHTML
 
-fun highlight(tokens: List<Token>): String = createHTML(prettyPrint = false).code {
+fun highlight(tokens: List<Token>): String = createHTML(prettyPrint = false).span {
     for (token in tokens) {
         val cls = when (token.type) {
             STRING -> "string"
             NUMBER -> "number"
             BYTES -> "bytes"
             COMMENT -> "comment"
+
             IF, ELSE, ELIF, WHILE, FOR, IN, NOT_IN, IS, IS_NOT, RETURN, AND, OR, NOT, BREAK, CONTINUE,
             FN, GLOBAL, LET, DO, END, ERROR, EXCEPT, FINALLY, RAISE
             -> "keyword"
 
-            else -> null
+            PLUS, MINUS, STAR, SLASH, PERCENT, RANGE, ELVIS, QUESTION_MARK, EQUALS, DOUBLE_EQUALS, NOT_EQUALS,
+            GREATER_THAN, LESS_THAN, GREATER_THAN_OR_EQUAL, LESS_THAN_OR_EQUAL
+            -> "operator"
+
+            DOT, COLON, COMMA, SEMICOLON -> "punctuation"
+
+            OPEN_PAREN, CLOSE_PAREN, OPEN_BRACE, CLOSE_BRACE, OPEN_BRACKET, CLOSE_BRACKET -> "bracket"
+
+            IDENTIFIER -> if (token.text in specialIdentifier) "keyword-literal" else "identifier"
+
+            WHITESPACE, EOF -> null
         }
-        span {
-            if (cls != null) {
-                classes = setOf("highlight", cls)
+        if (cls != null) {
+            span(classes = "highlight $cls") {
+                +token.text
             }
+        } else {
             +token.text
         }
     }
 }
+
+private val specialIdentifier = setOf("true", "false", "null")
---
+++ docs/gen.py
@@ -0,0 +1,54 @@
+import os
+import re
+import shutil
+import subprocess
+
+
+def highlight(match):
+    group = match.group(1)
+    proc = subprocess.Popen(
+        ["java", "-jar", "../app/build/libs/app-all.jar", "--syntax-highlight", "-c", group],
+        stdout=subprocess.PIPE
+    )
+    return proc.communicate()[0].decode("utf-8")
+
+
+metis_code = re.compile(r"<metis>(.*?)</metis>", re.DOTALL)
+dest = "../gendocs"
+
+if __name__ == "__main__":
+    os.chdir(os.path.dirname(os.path.dirname(__file__)))
+    os.system("./gradlew shadowJar")
+    os.chdir(os.path.dirname(__file__))
+    if not os.path.exists(dest):
+        os.mkdir(dest)
+
+    for dirpath, _, files in os.walk("."):
+        for file in files:
+            if not file.endswith(".papyri") or file.endswith(".lib.papyri"):
+                continue
+
+            with open(f"{dirpath}/{file}", "r") as f:
+                content = f.read()
+
+            print(f"Processing {dirpath}/{file}")
+            proc = subprocess.Popen(
+                ["papyri", "-i"],
+                stdout=subprocess.PIPE,
+                stderr=subprocess.PIPE,
+                stdin=subprocess.PIPE
+            )
+            content, stderr = proc.communicate(input=content.encode("utf-8"))
+            if stderr:
+                print(stderr.decode("utf-8"))
+                exit(1)
+            content = metis_code.sub(highlight, content.decode("utf-8"))
+            file = file.replace(".papyri", ".html")
+            dirpath = f"{dest}/{dirpath}"
+            if not os.path.exists(dirpath):
+                os.makedirs(dirpath)
+            with open(f"{dirpath}/{file}", "w") as f:
+                f.write(content)
+    if os.path.exists(f"{dest}/static"):
+        shutil.rmtree(f"{dest}/static")
+    shutil.copytree("static", f"{dest}/static")
